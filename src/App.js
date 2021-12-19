f596af84c245363301396ffcced5ab38b4109237
jehingson <jehingson25@gmail.com>
2021-12-19 11:48:25 -0500

    [fix] firts commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+
 import './App.css';
 
+const GIFS = ['https://media1.giphy.com/media/sULKEgDMX8LcI/giphy.gif?cid=1d93825a5dpld1u5ts4pvbrgbcdp99dbtnkn8vn117w2revz&rid=giphy.gif&ct=g', 'https://media0.giphy.com/media/eePSFNBFv2W9owZ4Sh/giphy.webp?cid=ecf05e477d9qvka3wzxca5lh4c8drr091sl3e5s7ewb4jlt4&rid=giphy.webp&ct=g']
+
+const DIFERENT_GIFS = ['https://media4.giphy.com/media/HXigA2Cvi4pZS/200w.webp?cid=ecf05e47t8at6tjp93yl7r4qcgb2m85r5pg6d4y0xmj03009&rid=200w.webp&ct=g', 'https://media4.giphy.com/media/2IQZZnJB9KHhJ9W7o1/100.webp?cid=ecf05e47t8at6tjp93yl7r4qcgb2m85r5pg6d4y0xmj03009&rid=100.webp&ct=g']
+
 function App() {
+
+  const [gifs, setGifs] = useState(GIFS)
+
+
+  useEffect(function () {
+    setGifs(DIFERENT_GIFS)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        {
+          gifs.map((singleGif, index) => <img key={index} src={singleGif} />)
+        }
+      </section>
     </div>
   );
 }
