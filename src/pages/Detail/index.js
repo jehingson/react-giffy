8d8a2ada87a5c6d1fcb224c5e1cfc1b85ec7c2b1
jehingson <jehingson25@gmail.com>
2021-12-21 15:39:55 -0500

    [fix] Custom Hooks blobal

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,15 +1,11 @@
 import React, { useContext } from 'react'
 import Gif from '../../components/Gif'
-import GifsContext from '../../context/GifsContext'
-import StaticContext from '../../context/StaticContext'
-
+import useGlobalGifs from '../../hooks/useGlobalGifs'
 
 export default function Detail({ params }) {
 
-    const { gifs } = useContext(GifsContext)
+    const gifs = useGlobalGifs()
     const gif = gifs.find(singleGif => singleGif.id === params.id)
-    console.log('context gifs', gif)
-    console.log('params', params.id)
 
     return <Gif {...gif} />
 }
